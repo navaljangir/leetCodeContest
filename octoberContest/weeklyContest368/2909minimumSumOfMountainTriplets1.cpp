67d39d473d337969eb4a80cf38c7c17b8d79063c
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-10-22 21:24:46 +0530

    Create 2909minimumSumOfMountainTriplets1.cpp

---
+++ octoberContest/weeklyContest368/2909minimumSumOfMountainTriplets1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumSum(vector<int>& nums) {
+        int mini = INT_MAX;
+        for(int i = 0; i<nums.size();i++){
+            for(int j = i+1 ; j<nums.size() ;j++){
+                for(int k = j+1; k<nums.size();k++){
+                    if(nums[i] <nums[j] && nums[k]<nums[j] )
+                    mini = min(mini , nums[i]+nums[j]+nums[k]);
+                }
+            }
+        }
+        if(mini==INT_MAX){
+            return -1;
+            
+        }
+        return mini;
+    }
+};
