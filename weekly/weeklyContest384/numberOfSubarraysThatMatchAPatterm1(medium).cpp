65a6f718e60bf5d25d2efdd5a4beceaef3293052
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2024-02-26 09:28:54 +0530

    Update numberOfSubarraysThatMatchAPatterm1(medium).cpp

--- weekly/weeklyContest384/numberOfSubarraysThatMatchAPatterm1(medium).cpp
+++ weekly/weeklyContest384/numberOfSubarraysThatMatchAPatterm1(medium).cpp
@@ -24,3 +24,47 @@ public:
         return ans;
     }
 };
+
+
+
+
+//Method 2 : string substr
+
+class Solution {
+public:
+    int countMatchingSubarrays(vector<int>& nums, vector<int>& pattern) {
+        string str1 = "";
+        for(int i = 0; i < nums.size()-1;i++){
+            if(nums[i]<nums[i+1]){
+                str1+=('1');
+            }else if(nums[i]==nums[i+1]){
+                str1+=('0');
+            }else if(nums[i]>nums[i+1]){
+                str1+=('2');
+            }
+        }
+        string str2 = "";
+        for(int i= 0 ; i <pattern.size();i++){
+            if(pattern[i]==-1){
+                str2+=('2');
+            }else{
+                str2+=(pattern[i]+'0');
+            }
+        }
+        int size = pattern.size();
+        int ans =0 ; 
+        string s = str1.substr(0 , size);
+        if(s==str2){
+            ans++;
+        } 
+        for(int i= size; i <str1.length();i++){
+            s = s.substr(1);
+            s+=str1[i];
+            if(s==str2){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
+
